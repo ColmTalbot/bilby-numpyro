2cd494bc0bdeb028f4407fad2b29485b63246930
Colm Talbot <talbotcolm@gmail.com>
2025-11-05 16:40:35 -0500

    DOC: add docstrings

--- bilby_numpyro/utils.py
+++ bilby_numpyro/utils.py
@@ -2,8 +2,35 @@ import jax.numpy as jnp
 from numpyro import deterministic, sample
 from numpyro.distributions import Normal, Unit
 
+__all__ = [
+    "bilby_to_numpyro_prior",
+    "bilby_to_numpyro_priors",
+    "generic_bilby_likelihood_function",
+    "construct_numpyro_model",
+    "sample_prior",
+]
+
 
 def bilby_to_numpyro_prior(value):
+    """
+    Convert a :code:`bilby` prior to a :code:`numpyro` prior.
+
+    This function attempts to match the prior class to a :code:`numpyro` prior class.
+    If no match is found, it defaults to a uniform prior.
+
+    Some features are not yet implemented, including constraints and conditional priors.
+
+    Parameters
+    ==========
+    value: bilby.core.prior.Prior
+        The prior to convert.
+
+    Returns
+    =======
+    numpyro.distributions.Distribution
+        The converted prior.
+    """
+
     from numpyro.distributions import (
         Cauchy,
         Chi2,
@@ -52,6 +79,20 @@ def bilby_to_numpyro_prior(value):
 
 
 def bilby_to_numpyro_priors(priors):
+    """
+    Convert a :code:`bilby.core.prior.PriorDict` to a dictionary of :code:`numpyro`
+    priors.
+
+    Parameters
+    ==========
+    priors: bilby.core.prior.PriorDict
+        The priors to convert in :code:`bilby` format.
+
+    Returns
+    =======
+    dict[str, numpyro.distributions.Distribution]
+        The converted priors in :code:`numpyro` format.
+    """
     jpriors = dict()
     for key, value in priors.items():
         jpriors[key] = bilby_to_numpyro_prior(value)
@@ -59,16 +100,48 @@ def bilby_to_numpyro_priors(priors):
 
 
 def generic_bilby_likelihood_function(likelihood, parameters, use_ratio=True):
-    likelihood.parameters.update(parameters)
+    """
+    A wrapper to allow a :code:`Bilby` likelihood to be used with :code:`jax`.
+
+    Parameters
+    ==========
+    likelihood: bilby.core.likelihood.Likelihood
+        The likelihood to evaluate.
+    parameters: dict
+        The parameters to evaluate the likelihood at.
+    use_ratio: bool, optional
+        Whether to evaluate the likelihood ratio or the full likelihood.
+        Default is :code:`True`.
+    """
     if use_ratio:
-        return likelihood.log_likelihood_ratio()
+        return likelihood.log_likelihood_ratio(parameters)
     else:
-        return likelihood.log_likelihood()
+        return likelihood.log_likelihood(parameters)
 
 
 def construct_numpyro_model(
     likelihood, priors, likelihood_func=generic_bilby_likelihood_function, **kwargs
 ):
+    """
+    Construct a :code:`numpyro` compatible model from :code:`bilby` likelihood and priors.
+
+    Parameters
+    ==========
+    likelihood: bilby.core.likelihood.Likelihood
+        The likelihood to evaluate.
+    priors: bilby.core.prior.PriorDict
+        The priors to use for inference.
+    likelihood_func: callable, optional
+        The function to use to evaluate the likelihood. Default is
+        :func:`gwpopulation.experimental.jax.generic_bilby_likelihood_function`.
+    kwargs: dict, optional
+        Additional keyword arguments to pass to the likelihood function.
+
+    Returns
+    =======
+    callable
+        The :code:`numpyro` model.
+    """
     from bilby.core.prior import DeltaFunction
 
     priors = priors.copy()
@@ -80,15 +153,26 @@ def construct_numpyro_model(
 
     def model():
         parameters = sample_prior(priors, delta_fns)
-        ln_l = jnp.nan_to_num(
-            likelihood_func(likelihood, parameters, **kwargs), nan=-jnp.inf
+        ln_l = generic_bilby_likelihood_function(
+            likelihood, priors, use_ratio=kwargs.get("use_ratio", True)
         )
+        ln_l = jnp.nan_to_num(ln_l, nan=-jnp.inf)
         sample("log_likelihood", Unit(ln_l), obs=ln_l)
 
     return model
 
 
 def sample_prior(priors, delta_fns):
+    """
+    Draw a sample from the :code:`numpyro` prior distribution.
+
+    Parameters
+    ==========
+    priors: dict[str, numpyro.distributions.Distribution]
+        The priors to sample from.
+    delta_fns: list[str]
+        The list of keys that are delta functions.
+    """
     parameters = dict()
     base = Normal(0, 1)
     for k, v in priors.items():
